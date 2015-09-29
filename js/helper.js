c3accbeeb0366888c8ea3166b9812d52c6325673
ewans05 <ewans05@gmail.com>
2015-09-28 21:36:57 -0400

    Update bio location push to map (revert to old line of code)

--- js/helper.js
+++ js/helper.js
@@ -127,7 +127,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array 
-    locations.push(bio.bioInfo[item].contacts[item2].location);
+    locations.push(bio.contacts.location);
 
     // iterates through school locations and appends each location to
     // the locations array
