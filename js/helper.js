cca06f25c3e9471aef99d63d4471e7390a7f7d8e
ewans05 <ewans05@gmail.com>
2015-10-01 23:41:00 -0400

    Remove a tag from onlineurl

--- js/helper.js
+++ js/helper.js
@@ -54,7 +54,7 @@ var HTMLonlineClasses = '<h3 id="online-class">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><div>%data%</div>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
