0e590163d8a7ac8fd3934fd6a218355ba112910f
ewans05 <ewans05@gmail.com>
2015-10-01 23:06:19 -0400

    Edit skills and project descriptions. Add links to work and project titles.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,17 +4,17 @@
 
 var bio = {
 	'name' : 'Sasha Ewan',
-	'role' : 'Web Developer',
-	contacts : 
+	'role' : 'Web Ninja',
+	'contacts' : 
 		{
 		'github' : 'ewans05',
 		'mobile' : '555-5500',
 		'email' : 'ewans05@gmail.com',
 		'location' : 'Bronx, NY'
 		},
-	'picture' : 'images/fry.jpg',
+	'picture' : 'images/photo-sasha1.jpg',
 	'welcomeMessage' : 'I do solemnly swear that I am up to no good',
-	'skills' : ['laughing at my own jokes', 'napping', 'nunchucks']
+	'skills' : ['laughing at my own jokes', 'napping', 'enthusiasm for Harry Potter']
 };
 
 bio.display = function() {
@@ -53,14 +53,14 @@ bio.display();
 
 /*****LAST NAME CAPS BUTTON*****/
 
-$("#main").append(internationalizeButton);
+/*$("#main").append(internationalizeButton);
 function inName(name) {
 	var name = bio.name.split(" ");
 	var lastNameCap = name[1].toUpperCase();
 	var firstName = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
 	fullName = firstName + " " + lastNameCap;
 	return fullName;
-};
+};*/
 
 /******BODY******/
 
@@ -69,25 +69,25 @@ function inName(name) {
 var work = {
 	'jobs' : [
 		{
-			'company' : 'LCAM LLC',
+			'company' : '<a href="http://www.lcamllc.com/" target="_blank">Lion Cao Asset Management LLC</a>',
 			'city' : 'New York, NY',
-			'title' : 'El Jefe',
+			'title' : 'Assistant, Operations & Administration',
 			'dates' : '2005 - present',
-			'description' : 'coordinating and facilitating'
+			'description' : 'Coordinating with investors to streamline the due diligence process, ensure all documentation is executed, and deals are finalized. Processing payments of bills and invoices, tracking expenditures and constructing expense reports for reimbursement.'
 		},
 		{
-			'company' : 'TF Corner',
-			'city' : 'New York, NY',
-			'title' : 'Liason',
-			'dates' : '2004 - 2005',
-			'description' : 'real-estating type things'
+			'company' : '<a href="https://tfc.com/" target="_blank">TF Cornerstone</a>',
+			'city' : 'Long Island City, NY',
+			'title' : 'Leasing Office Agent',
+			'dates' : '2009 - 2010',
+			'description' : 'Greeted and ascertained needs of prospective clients, determining if they would be good occupants, followed up upon completion of application. Knowledgeable on all aspects of each property available, from physical dimensions to amenities and nearby services.'
 		},
 		{
-			'company' : 'Productions LLC',
+			'company' : '<a href="http://mcneilgrayandrice.com/" target="_blank">McNeil, Gray & Rice Public Relations</a>',
 			'city' : 'Boston, MA',
-			'title' : 'Minion',
-			'dates' : '2003 - 2004',
-			'description' : 'picking things up and then putting them down'
+			'title' : 'Intern',
+			'dates' : '2009',
+			'description' : 'Researched technology organizations for business-to-business media campaigns. Compiled lists of potential clients in targeted field.'
 		}
 	]
 };
@@ -180,16 +180,16 @@ education.display();
 var projects = {
 	'udacityWork': [
 		{
-		'title' : 'P0',
-		'dates' : 'July 2015',
-		'description' : 'You will get acquainted with your text editor, creating and editing simple HTML and CSS files to create your very first webpage! You should be able to complete this project after reviewing Lesson 1 of the Intro to HTML & CSS course.',
-		'image' : ['http://blog.udacity.com/wp-content/uploads/2013/07/Udacity_Logo.png']
-		},
-		{
-		'title' : 'P1',
+		'title' : '<a href="http://htmlpreview.github.io/?https://github.com/ewans05/Project-P1/blob/master/index.html" target="_blank">P1</a>',
 		'dates' : 'Aug 2015',
 		'description' : 'You will be provided with a design mockup as a PDF-file and must replicate that design in HTML and CSS. You will develop a responsive website that will display images, descriptions and links to each of the portfolio projects you will complete throughout the course of the Front-End Web Developer Nanodegree.',
-		'image' : ['http://s3.amazonaws.com/s3.userdata.www.universalsubtitles.org/teams/logo/cd2825021f6c59ca5d4dc5019a43d6896363783e.png']
+		'image' : ['https://d2q0qd5iz04n9u.cloudfront.net/_ssl/proxy.php/http/gallery.mailchimp.com/0c10b5b87df9a80d37817b056/images/udacity_logo_thumb553593.jpg']
+		},
+		{
+		'title' : 'P2',
+		'dates' : 'Sept 2015',
+		'description' : 'You will demonstrate your mastery of the language\'s syntax through a series of challenges. Each multipart problem mimics a real-life challenge that front-end developers face. You\'ll be required to write clean code and to apply your knowledge of variables, objects, JSON, functions and control flow to successfully solve the challenges.',
+		'image' : ['http://www.technapex.com/wp-content/uploads/2012/10/Udacity560.jpg']
 		}
 	]
 };
